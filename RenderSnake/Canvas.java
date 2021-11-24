96e55027e57ee2263efa958121d15ce0369508f9
xiao278 <xiao278@purdue.edu>
2021-11-24 15:00:51 -0500

    Worm render

---
+++ RenderSnake/Canvas.java
@@ -0,0 +1,91 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.util.ArrayList;
+
+public class Canvas extends JComponent implements Runnable {
+
+    private Graphics2D graphics2D;
+    Image image;
+    int xPos;
+    int yPos;
+    Point mousePos;
+    public ArrayList<Drawable> drawables;
+
+    public Canvas() {
+        drawables = new ArrayList<>();
+        addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                mousePos = e.getPoint();
+            }
+        });
+
+        addMouseMotionListener(new MouseMotionAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                xPos += e.getX() - mousePos.x;
+                yPos += e.getY() - mousePos.y;
+                mousePos = e.getPoint();
+                repaint();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Canvas());
+    }
+
+    public void run(){
+        JFrame frame = new JFrame();
+        Container content = frame.getContentPane();
+        Canvas canvas = new Canvas();
+
+        content.add(canvas, BorderLayout.CENTER);
+        frame.setSize(600, 400);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+
+        var mc = new MovingCircle();
+        //var s = new Snake(30,10);
+        var s2 = new TraceFixedPoint(14,50,0.1f);
+        //canvas.addDrawable(mc);
+        canvas.addDrawable(s2);
+    }
+
+    protected void paintComponent(Graphics g) {
+        if (image == null) {
+            image = createImage(getSize().width, getSize().height);
+
+            // this lets us draw on the image (ie. the canvas)
+            graphics2D = (Graphics2D) image.getGraphics();
+            // gives us better rendering quality for the drawing lines
+            graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+
+            // set canvas to white with default paint color
+            graphics2D.setPaint(Color.white);
+            graphics2D.fillRect(0, 0, getSize().width, getSize().height);
+            graphics2D.setPaint(Color.black);
+            repaint();
+        }
+        for (Drawable drawable : drawables) {
+            drawable.draw(this.graphics2D);
+        }
+        g.drawImage(image, 0, 0, null);
+        clear();
+    }
+    private void clear() {
+        graphics2D.setPaint(Color.white);
+        graphics2D.fillRect(0, 0, getSize().width, getSize().height);
+        graphics2D.setPaint(Color.black);
+    }
+
+    public void addDrawable(Drawable drawable){
+        drawables.add(drawable);
+        drawable.setC(this);
+    }
+}
---
+++ RenderSnake/Vector2.java
@@ -0,0 +1,44 @@
+public class Vector2 {
+    public double x;
+    public double y;
+    Vector2(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    Vector2(){
+        this.x = 0;
+        this.y = 0;
+    }
+    public double len(){
+        return Math.sqrt(x*x + y*y);
+    }
+    public Vector2 unitV(){
+        double len = len();
+        return new Vector2(x/len,y/len);
+    }
+
+    //this to input vecotr
+    public Vector2 to(Vector2 v){
+        return new Vector2(v.x - x, v.y - y);
+    }
+
+    public Vector2 from(Vector2 v){
+        return new Vector2(x - v.x, y - v.y);
+    }
+
+    public void set(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector2 add(Vector2 v){
+        return new Vector2(v.x + x, v.y + y);
+    }
+
+    public Vector2 scale(double scalar){
+        return new Vector2(x*scalar, y*scalar);
+    }
+    public String toString(){
+        return "(" + this.x + ", " + this.y + ")";
+    }
+}
