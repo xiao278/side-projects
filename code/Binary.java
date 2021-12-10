3335b11aa1a091a49ad433d5ac8b902b1595e1ae
xiao278 <90475159+xiao278@users.noreply.github.com>
2021-12-10 04:10:45 -0500

    [Binary] method references and xor function

--- code/Binary.java
+++ code/Binary.java
@@ -7,7 +7,7 @@ public class Binary {
         while(in > 0){
             int remainder = in%2;
             in = in/2;
-            binary.add(0,remainder == 1);
+            binary.add(remainder == 1);
         }
     }
 
@@ -17,20 +17,21 @@ public class Binary {
     public String toString(){
         String s = "";
         for(int i = 0; i < binary.size(); i++){
-            boolean temp = binary.get(i);
+            boolean temp = getDigit(i);
             if(temp) {
                 s += "1";
             } else {
                 s += "0";
             }
         }
+        s = s.replaceFirst("^0", "");
         return s;
     }
 
     public static void main(String[] args) {
-        var b = new Binary(69);
-        var a = new Binary(80);
-        var c = b.or(a);
+        var b = new Binary(155);
+        var a = new Binary(235);
+        var c = a.xor(b);
         System.out.println(b);
         System.out.println(a);
         System.out.println(c);
@@ -40,35 +41,55 @@ public class Binary {
         return binary.get(binary.size()-index-1);
     }
 
-    public Binary and(Binary b){
+    private Binary operation(Binary b, I bridge){
+        var result = new ArrayList<Boolean>();
         var tempA = this.getBinary();
         var tempB = b.getBinary();
-        var result = new ArrayList<Boolean>();
         for(int i = 0; i < Math.min(tempA.size(),tempB.size()); i++){
-            result.add(0,this.getDigit(i) && b.getDigit(i));
+            result.add(bridge.method(tempA.get(i),tempB.get(i)));
+        }
+        ArrayList<Boolean> bigger;
+        if(tempA.size() > tempB.size()){
+            bigger = tempA;
+        }
+        else bigger = tempB;
+        for(int i = Math.min(tempA.size(),tempB.size()); i < Math.max(tempA.size(), tempB.size()); i++){
+            result.add(bridge.method(bigger.get(i), false));
         }
         return new Binary(result);
     }
 
+    public Binary and(Binary b){
+        return operation(b, Binops::and);
+    }
+
     public Binary or(Binary b){
-        var result = new ArrayList<Boolean>();
-        var aLength = this.getBinary().size();
-        var bLength = b.getBinary().size();
-        for(int i = 0; i < Math.min(bLength,aLength); i++){
-            result.add(0,this.getDigit(i) || b.getDigit(i));
-        }
-        Binary bigger;
-        if(aLength > bLength){
-            bigger = this;
-        }
-        else bigger = b;
-        for(int i = Math.min(bLength,aLength); i < Math.max(bLength,aLength); i++){
-            result.add(0,bigger.getDigit(i));
-        }
-        return new Binary(result);
+        return operation(b, Binops::or);
+    }
+
+    public Binary xor(Binary b){
+        return operation(b, Binops::xor);
     }
 
     public ArrayList<Boolean> getBinary() {
         return binary;
+    }
+}
+
+interface I{
+    public Boolean method(boolean a, boolean b);
+}
+
+class Binops{
+    public static boolean and(boolean a,boolean b){
+        return a && b;
+    }
+
+    public static boolean or(boolean a,boolean b){
+        return a || b;
+    }
+
+    public static boolean xor(boolean a, boolean b){
+        return a != b;
     }
 }
